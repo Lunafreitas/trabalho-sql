e8d6acdf1fbd31ca20f06688b9f12b07388a7deb
Lunafreitas <luna.freitas5@aluno.ce.gov.br>
2026-09-22 20:07:39 -0300

    criei as tabelas, falta os pontos 3, 4, 5, 6 e os outros 2 slides

---
+++ trabalho-sql/clinica.sql
@@ -0,0 +1,47 @@
+CREATE DATABASE Clinica;
+USE Clinica;
+
+CREATE TABLE Ambulatorios (
+    nroa INT PRIMARY KEY,
+    andar NUMERIC(3) NOT NULL,
+    capacidade SMALLINT
+);
+
+CREATE TABLE Medicos (
+    codm INT PRIMARY KEY,
+    nome VARCHAR(40) NOT NULL,
+    idade SMALLINT NOT NULL,
+    especialidade CHAR(20),
+    CPF NUMERIC(11) UNIQUE,
+    cidade VARCHAR(30),
+    nroa INT,
+    FOREIGN KEY (nroa) REFERENCES Ambulatorios(nroa) -- chave estrangeira ai, references indica qual tabel e coluna a FK referee
+);
+
+CREATE TABLE Pacientes (
+    codp INT PRIMARY KEY,
+    nome VARCHAR(40) NOT NULL,
+    idade SMALLINT NOT NULL,
+    cidade CHAR(30),
+    CPF NUMERIC(11) UNIQUE,
+    doenca VARCHAR(40) NOT NULL
+);
+
+CREATE TABLE Funcionarios (
+    codf INT PRIMARY KEY,
+    nome VARCHAR(40) NOT NULL,
+    idade SMALLINT,
+    CPF NUMERIC(11) UNIQUE,
+    cidade VARCHAR(30),
+    salario NUMERIC(10),
+    cargo VARCHAR(20)
+);
+
+CREATE TABLE Consultas (
+    codm INT,
+    codp INT,
+    data DATE,
+    hora TIME,
+    FOREIGN KEY (codm) REFERENCES Medicos(codm),
+    FOREIGN KEY (codp) REFERENCES Pacientes(codp)
+);
