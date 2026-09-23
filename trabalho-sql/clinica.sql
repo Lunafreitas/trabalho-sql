0496c8909607c350de23c7ffafe1911bea2882b3
Lunafreitas <luna.freitas5@aluno.ce.gov.br>
2026-09-22 22:35:46 -0300

    tabelas e INSERTS criados, falta só o UPDATE (último slide)

--- trabalho-sql/clinica.sql
+++ trabalho-sql/clinica.sql
@@ -55,3 +55,42 @@ CREATE INDEX idx_pacientes_doenca ON Pacientes(doenca);
 DROP INDEX idx_pacientes_doenca ON Pacientes;
 
 ALTER TABLE Funcionarios DROP COLUMN cargo, DROP COLUMN nroa;
+
+
+
+
+
+INSERT INTO Ambulatorios (nroa, andar, capacidade) VALUES (1, 1, 30);
+INSERT INTO Ambulatorios (nroa, andar, capacidade) VALUES (2, 1, 50);
+INSERT INTO Ambulatorios (nroa, andar, capacidade) VALUES (3, 2, 40);
+INSERT INTO Ambulatorios (nroa, andar, capacidade) VALUES (4, 2, 25);
+INSERT INTO Ambulatorios (nroa, andar, capacidade) VALUES (5, 2, 55);
+
+INSERT INTO Medicos (codm, nome, idade, especialidade, CPF, cidade, nroa) VALUES (1, 'João', 40, 'ortopedia', 10000100000, 'Florianopolis', 1);
+INSERT INTO Medicos (codm, nome, idade, especialidade, CPF, cidade, nroa) VALUES (2, 'Maria', 42, 'traumatologia', 10000110000, 'Blumenau', 2);
+INSERT INTO Medicos (codm, nome, idade, especialidade, CPF, cidade, nroa) VALUES (3, 'Pedro', 51, 'pediatria', 11000100000, 'São José', 2);
+INSERT INTO Medicos (codm, nome, idade, especialidade, CPF, cidade, nroa) VALUES (4, 'Carlos', 28, 'ortopedia', 11000110000, 'Joinville', 4);
+INSERT INTO Medicos (codm, nome, idade, especialidade, CPF, cidade, nroa) VALUES (5, 'Marcia', 33, 'neurologia', 11000111000, 'Biguacu', 3);
+
+INSERT INTO Pacientes (codp, nome, idade, cidade, CPF, doenca) VALUES (1, 'Ana', 20, 'Florianopolis', 20000200000, 'gripe');
+INSERT INTO Pacientes (codp, nome, idade, cidade, CPF, doenca) VALUES (2, 'Paulo', 24, 'Palhoca', 20000222000, 'fratura');
+INSERT INTO Pacientes (codp, nome, idade, cidade, CPF, doenca) VALUES (3, 'Lucia', 30, 'Biguacu', 22000200000, 'tendinite');
+INSERT INTO Pacientes (codp, nome, idade, cidade, CPF, doenca) VALUES (4, 'Carlos', 28, 'Joinville', 11000110000, 'sarampo');
+
+INSERT INTO Funcionarios (codf, nome, idade, cidade, salario, CPF) VALUES (1, 'Rita', 32, 'Sao Jose', 1200, 20000100000);
+INSERT INTO Funcionarios (codf, nome, idade, cidade, salario, CPF) VALUES (2, 'Maria', 55, 'Palhoca', 1220, 30000110000);
+INSERT INTO Funcionarios (codf, nome, idade, cidade, salario, CPF) VALUES (3, 'Caio', 45, 'Florianopolis', 11000100000);
+INSERT INTO Funcionarios (codf, nome, idade, cidade, salario, CPF) VALUES (4, 'Carlos', 44, 'Florianopolis', 1200, 51000110000);
+INSERT INTO Funcionarios (codf, nome, idade, cidade, salario, CPF) VALUES (5, 'Paula', 33, 'Florianopolis', 2500, 61000110000);
+
+INSERT INTO Consultas (codm, codp, data, hora) VALUES (1, 1, '2006-06-12', '14:00:00'),
+INSERT INTO Consultas (codm, codp, data, hora) VALUES (1, 4, '2006-06-13', '10:00:00'),
+INSERT INTO Consultas (codm, codp, data, hora) VALUES (2, 1, '2006-06-13', '09:00:00'),
+INSERT INTO Consultas (codm, codp, data, hora) VALUES (2, 2, '2006-06-13', '11:00:00'),
+INSERT INTO Consultas (codm, codp, data, hora) VALUES (2, 3, '2006-06-14', '14:00:00'),
+INSERT INTO Consultas (codm, codp, data, hora) VALUES (2, 4, '2006-06-14', '17:00:00'),
+INSERT INTO Consultas (codm, codp, data, hora) VALUES (3, 1, '2006-06-19', '18:00:00'),
+INSERT INTO Consultas (codm, codp, data, hora) VALUES (3, 3, '2006-06-12', '10:00:00'),
+INSERT INTO Consultas (codm, codp, data, hora) VALUES (3, 4, '2006-06-19', '13:00:00'),
+INSERT INTO Consultas (codm, codp, data, hora) VALUES (4, 4, '2006-06-20', '13:00:00'),
+INSERT INTO Consultas (codm, codp, data, hora) VALUES (4, 4, '2006-06-22', '19:30:00');
